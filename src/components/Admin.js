65a73f57cd2c7252c439ccb2de63f25d47d70f17
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-08-06 23:48:40 -0500

    display movies in Admin Catelogue to be edited or deleted

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,10 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Admin extends Component {
 
+    // set state
+    state = {
+        movies: [], 
+        isLoaded: false, 
+        error: null,
+    }; 
+
+    componentDidMount() {
+        // get movies 
+        fetch("http://localhost:4000/v1/movies")
+            .then((response) => {
+                if (response.status !== "200") {
+                    let err = Error; 
+                    err.message = "Invalid response code " + response.status;
+                    // set error in state 
+                    this.setState({error: err});
+                }
+                return response.json();
+            })
+            // if no error, convert response to json 
+            .then((json) => {
+                this.setState({
+                    movies: json.movies,
+                    isLoaded: true, 
+                }, 
+                (error) => {
+                    this.setState({isLoaded: true, error});
+                })
+            })
+    }
+
     render() {
-        return (
-            <h2>Admin portal</h2>
-        )
+        const { movies, isLoaded, error } = this.state; 
+        
+        if (error) {
+            return <div>Error: {error.message}</div>
+        } else if (!isLoaded) {
+            return <p>Loading...</p>
+        } else {
+            return (
+                <Fragment>
+                    <h2>Manage Catalogue</h2>
+                    <div className="list-group">
+                        {/* display list of movies */}
+                        {movies.map((m) => (
+                            <Link
+                                to={`/admin/movie/${m.id}`}
+                                key={m.id}
+                                className="list-group-item list-group-item-action"
+                            >
+                                {m.title}
+                            </Link>
+                        ))}
+                    </div>
+                </Fragment>
+            )
+        }
     }
 }
