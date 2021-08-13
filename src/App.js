82e320e73657701a9e7389b393690d42cd54ecd9
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-08-13 15:51:28 -0500

    replace Generes class with GenresFuncs which uses hooks

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Movies from './components/Movies'
 import Admin from './components/Admin'
 import Home from './components/Home'
 import OneMovie from './components/OneMovie';
-import Genres from './components/Genres';
 import OneGenre from './components/OneGenre';
 import EditMovie from './components/EditMovie';
 import Login from './components/Login'
 import GraphQL from './components/GraphQL';
 import OneMovieGraphQL from './components/OneMovieGraphQL';
+// import Genres from './components/Genres';
+import GenresFunc from './components/GenresFunc';
 
 export default class App extends Component {
   constructor(props) {
@@ -139,7 +140,7 @@ export default class App extends Component {
 
               {/* forces the router to match exact path */}
               <Route exact path="/genres">
-                <Genres />
+                <GenresFunc />
               </Route>
 
               <Route exact path="/graphql">
---
+++ src/components/GenresFunc.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+
+function GenresFunc(props) {
+    const [genres, setGenres] = useState([]);
+    const [error, setError] = useState(null); 
+    const [isLoaded, setLoaded] = useState(false); 
+
+    // use useEffect in place of componentDidMount()
+    useEffect(() => {
+        // fetch genres 
+        fetch(`${process.env.REACT_APP_API_URL}/v1/genres`)
+            .then((response) => {
+                if (response.status !== 200) {
+                    setError("Invalid response: ", response.status);
+                    setLoaded(true);
+                } else {
+                    // no error
+                    setError(null);
+                }
+                return response.json();
+            })
+            .then((json) => {
+                setGenres(json.genres);
+                setLoaded(true);
+            });
+    },[]);
+
+    // render 
+    if (error) { // if has error
+        return <div>Error: {error.message}</div>
+    } else if (!isLoaded) { // if not loaded
+        return <p>Loading...</p>
+    } else {
+        return (
+            <Fragment> 
+                <h2>Genres</h2>
+
+                <div className="list-group">
+                    {/* loop through genres */}
+                    {genres.map((m) => (
+                        <Link
+                            key={m.id}
+                            className="list-group-item list-group-item-action"
+                            to={{
+                                pathname: `/genre/${m.id}`, 
+                                genreName: m.genre_name,
+                            }}
+                        >
+                            {m.genre_name}
+                        </Link>
+                    ))}
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default GenresFunc;
