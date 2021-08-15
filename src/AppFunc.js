5cc595f1d982387ca4c26a1b989343c8e504fe93
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-08-14 23:45:45 -0500

    add remaining App functions to AppFunc and modify as needed

--- src/AppFunc.js
+++ src/AppFunc.js
@@ -27,4 +27,142 @@ export default function AppFunc(props) {
           }
         }
     }, [jwt])
+
+    function handlejwtChange(jwt) {
+        setJwt({jwt: jwt});
+    }
+
+    function logout() {
+        setJwt("");
+        // clear local storage 
+        window.localStorage.removeItem("jwt");
+    }
+
+    let loginLink; 
+    if (jwt === "") {
+      loginLink = <Link to="/login">Login</Link>
+    } else {
+      loginLink = <Link to="/logout" onClick={logout}>Logout</Link>
+    }
+
+    return (
+      <Router>
+      <div className="container">
+        <div className="row">
+          <div className="col mt-3">
+            <h1 className="mt-3">
+              Go Watch a Movie!
+            </h1>
+          </div>
+
+          <div className="col mt-3 text-end">
+            {loginLink}
+          </div>
+          <hr className="mb-3"></hr>
+        </div>
+
+        {/* content */}
+        <div className="row">
+          <div className="col-md-2">
+            <nav>
+              <ul className="list-group">
+                {/* LINKS */}
+                <li className="list-group-item">
+                  {/* <a href="/">Home</a> */}
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/movies">Movies</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/genres">Genres</Link>
+                </li>
+
+                {/* check if jwt is empty */}
+                {jwt !== "" && (
+                  <Fragment>
+                    <li className="list-group-item">
+                      <Link to="/admin/movie/0">Add Movie</Link>
+                    </li>
+                    <li className="list-group-item">
+                      <Link to="/admin">Manage Catalogue</Link>
+                    </li>
+                  </Fragment>
+                )}
+                <li className="list-group-item">
+                  <Link to="/graphql">GraphQL</Link>
+                </li>
+
+              </ul>
+              <pre>
+                {/* display state */}
+                {JSON.stringify(state, null, 3)}
+              </pre>
+            </nav>
+          </div>
+
+          {/* main content */}
+          <div className="col-md-10">
+
+            {/* Matches LINKS */}
+            <Switch>
+
+              <Route exact path="/">
+                <Home />
+              </Route>
+
+              <Route path="/movies/:id" component={OneMovieFunc} />
+
+              <Route path="/moviesgraphql/:id" component={OneMovieGraphQL} />
+
+              <Route path="/movies">
+                <Movies />
+              </Route>
+
+              <Route path="/genre/:id" component={OneGenreFunc} />
+
+              {/* route with a props and bind with jwt to lift state */}
+              <Route exact path="/login" 
+                component={(props) => <LoginFunc {...props} handleJWTChangeFromParent={handleJWTChange} /> }/>
+
+              {/* forces the router to match exact path */}
+              <Route exact path="/genres">
+                <GenresFunc />
+              </Route>
+
+              <Route exact path="/graphql">
+                <GraphQL />
+              </Route>
+
+              {/* push jwt to the EditMovie component */}
+              {/* when id = 0, adding movie, else the movie of the id is being edited */}
+              {/* <Route path="/admin/movie/:id" component={EditMovie} /> */}
+              <Route path="/admin/movie/:id" component={(props) => (
+                <EditMovieFunc {...props} jwt={state.jwt} />
+              )} 
+              />
+
+              {/* <Route path="/by-category/comedy"
+                  render={(props) => <Categories {...props} title={`Comedy`} />}   
+              /> */}
+
+              {/* <Route path="/admin">
+                <Admin />
+              </Route> */}
+
+              <Route 
+                path="/admin"
+                component={(props) => (
+                  // push props and jwt to component 
+                  <AdminFunc {...props} jwt={state.jwt} />
+                )}
+              />
+
+            </Switch>
+
+          </div>
+        </div>
+      </div>
+    </Router>
+    )
 }
